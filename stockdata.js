ba8f53d44bd081f85470f13eb2afbf196c71822f
zl4624 <zl4624@ic.ac.uk>
2025-03-31 15:42:59 +0100

    Removed incrmrange

--- stockdata.js
+++ stockdata.js
@@ -1,6 +1,5 @@
 import yfinance from 'yahoo-finance2';
 import incrmmean from '@stdlib/stats/incr/mmean/lib/index.js';
-import incrmrange from '@stdlib/stats/incr/mrange/lib/index.js';
 import plot from '@stdlib/plot';
 import fs from 'fs';
 import path from 'path';
@@ -25,7 +24,7 @@ function getSlowFastAvg( quotes ) {
     const dates = [];
 
     quotes.forEach(quote => {
-        prices.push( quote.close );
+        prices.push( quote.close )
         slows.push( slow( quote.close ) );
         fasts.push( fast( quote.close ) );
         dates.push(new Date(quote.date).getTime());
@@ -61,18 +60,15 @@ async function main() {
         }
     );
 
-    var diff = incrmrange( 2 );
-    diff( prices[0] );
-
     var maprofit = 0;
     var simplebuyprofit = 0;
     const maarr = [maprofit]
     const simplebuyarr = [simplebuyprofit]
     for (let i = 1; i < prices.length; ++i) {
-        diff( prices[i] )
+        const diff = prices[i] - prices[i - 1];
 
-        simplebuyprofit += diff()
-        maprofit += slows[i] < fasts[i] ? diff() : 0;
+        simplebuyprofit += diff
+        maprofit += slows[i] < fasts[i] ? diff : 0;
         simplebuyarr.push( simplebuyprofit );
         maarr.push( maprofit );
     }
